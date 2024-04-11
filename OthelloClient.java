69a4e799e89b7d6864129d011693b613cdf5ffe8
bmeistergeneral <brecon.morgan@gmail.com>
2024-04-11 11:01:12 -0400

    Socket, BW, BR, Thread

--- OthelloClient.java
+++ OthelloClient.java
@@ -1,6 +1,17 @@
 import java.io.*;
-public class OthelloClient {
-public static void main(String[] args) {
+import java.net.*;
+import java.util.*;
 
-}
+public class OthelloClient {
+
+    class myThread extends Thread {
+        public void run() {
+            System.out.println("thread is running...");
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        Socket s = new Socket("localhost", 9999);
+        BufferedWriter write = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+        BufferedReader read = new BufferedReader(new InputStreamReader(s.getInputStream()));
+    }
 }
--- OthelloServer.java
+++ OthelloServer.java
@@ -1,5 +1,5 @@
 public class OthelloServer {
     public static void main(String[] args) {
-        
+
     }
 }
