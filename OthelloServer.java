60885038915f7ddfe99ff2494992f60fd814f28f
GreenMatt413 <111987332+GreenMatt413@users.noreply.github.com>
2024-04-26 14:00:44 -0400

    Add files via upload
    Added several things to the server, created board, can now communicate with client, started code to write the board state back to the client.

--- OthelloServer.java
+++ OthelloServer.java
@@ -2,40 +2,66 @@ import java.io.*;
 import java.net.*;
 
 public class OthelloServer {
-        public static void main(String[] args) throws IOException {
-            ServerSocket se = new ServerSocket(2048);
-            while (true) {
-                try {
-                    Socket so = se.accept();
+    public static void main(String[] args) throws IOException {
+        ServerSocket se = new ServerSocket(2048);
+    while(true){
+        try{
+            Socket so = se.accept();
 
-                    System.out.println("Connected to client!" + so);
+            System.out.println("Client Connected!"+so);
 
-                    BufferedReader br = new BufferedReader(new InputStreamReader(so.getInputStream()));
-                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(so.getOutputStream()));
+            BufferedReader br = new BufferedReader(new InputStreamReader(so.getInputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(so.getOutputStream()));
 
-                    Thread t = new ControlClient(so, br, bw);
+            Thread t = new ControlClient(so, br, bw);
 
-                    t.start();
-                } catch(Exception e){
-                    e.printStackTrace();
-                }
-
-            }
+            t.start();
         }
+        catch(Exception e){}
+
+    }
+    }
 }
-class ControlClient extends Thread {
+    class ControlClient extends Thread{
     final BufferedReader br;
-    final BufferedWriter bw;
-    final Socket so;
+        final BufferedWriter bw;
+        final Socket so;
 
-    public ControlClient(Socket s, BufferedReader br, BufferedWriter bw){
-        this.so = s;
-        this.br = br;
-        this.bw = bw;
+        public ControlClient(Socket s, BufferedReader br, BufferedWriter bw){
+            this.so = s;
+            this.br = br;
+            this.bw = bw;
+        }
+
+        public void run(){
+            while(true){
+                try{
+
+            bw.write("Starting Othello Game....... ");
+            bw.newLine();
+            bw.flush();
+
+            int[][] othelloBoard = new int[8][8];
+            othelloBoard[3][3]= 1; //Player 1
+            othelloBoard[4][4]= 1;
+            othelloBoard[3][4]= 2; //Player 2
+            othelloBoard[4][3]= 2;
+
+            bw.write("Test");
+            bw.newLine();
+            bw.flush();
+            // for (int i=0;i<othelloBoard.length;i++){
+            //     for (int j=0;j<othelloBoard.length;j++){
+            //         bw.write(othelloBoard[i][j]);
+            //     }
+            //     bw.newLine();
+            // }
+
+            System.out.println("Server Closed!");
+            this.br.close();
+            this.bw.close();
+                }
+            catch(IOException e){}
+        }
     }
-
-    public void run(){
-
     }
-
-}
